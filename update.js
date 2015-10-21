671a7fd3e67dafdaf209f4dadb071f738ee61294
zenoid <zen@zenoid.it>
2015-10-21 22:33:52 +0200

    Date formats fixed

--- update.js
+++ update.js
@@ -7,10 +7,8 @@ fs.readFile( locationsFile, 'utf-8', function( err, fileContent ) {
   if ( err ) throw err;
 
   var locations = JSON.parse( fileContent ),
-    daysList = Object.keys( locations ),
-    lastDay = new Date( daysList[ daysList.length - 1 ] ),
-    startingDay = new Date( lastDay.setDate( lastDay.getDate() + 1 ) );
+    daysList = Object.keys( locations );
 
-  parser.updateLocationData( startingDay );
+  parser.updateLocationData( new Date( daysList[ daysList.length - 1 ] ) );
 
 });
